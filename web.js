86eb8165cd3fe53c2755f3f83b698b160875095b
Sushma Sharma <sushma.sharma@dealertrack.com>
2014-08-14 13:47:28 -0400

    Changes to web.js

--- web.js
+++ web.js
@@ -5,8 +5,8 @@ var app = express.createServer(express.logger());
 app.use(gzippo.staticGzip(__dirname + '/dist'));
 app.listen(process.env.PORT || 5000);
 
-app.use forceSsl(req, res, next) ->
-if req.header 'x-forwarded-proto' != 'https'
-  res.redirect "https://#{req.header 'host'}#{req.url}"
-else
-  next()
+app.use forceSsl(req, res, next)
+    if req.header 'x-forwarded-proto' != 'https'
+      res.redirect "https://#{req.header 'host'}#{req.url}"
+    else
+      next()
