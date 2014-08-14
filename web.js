9fb3c64743adc817aca7147ffec53bc4559dbcc8
Sushma Sharma <sushma.sharma@dealertrack.com>
2014-08-14 15:59:06 -0400

    Revert https

--- web.js
+++ web.js
@@ -4,9 +4,3 @@ var express = require('express');
 var app = express.createServer(express.logger());
 app.use(gzippo.staticGzip(__dirname + '/dist'));
 app.listen(process.env.PORT || 5000);
-
-app.use forceSsl(req, res, next)
-    if req.header 'x-forwarded-proto' != 'https'
-      res.redirect "https://#{req.header 'host'}#{req.url}"
-    else
-      next()
