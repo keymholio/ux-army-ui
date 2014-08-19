602e94a1e35942028dc2672fe73924a8209c9d03
Andrew Keym <andrew.keym@gmail.com>
2014-08-19 14:48:50 -0400

    Changed express config and upgraded express in package.json

--- web.js
+++ web.js
@@ -4,10 +4,7 @@ var path = require('path');
 
 app = express();
 
-app.configure(function(){
-    app.use(express.bodyParser());
-    app.use(express.static(path.join(__dirname, 'dist')));
-});
+app.use(express.static(path.join(__dirname, 'dist')));
 
 var port = process.env.PORT || 5000;
 
