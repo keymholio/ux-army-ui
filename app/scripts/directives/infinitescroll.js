47a5d2646306c3eb905a323b10fc4ba1d572b8e9
molinamiguel <miguelmolina110@gmail.com>
2014-08-26 13:59:59 -0400

    Change pass directive for dash and participant pg

--- app/scripts/directives/infinitescroll.js
+++ app/scripts/directives/infinitescroll.js
@@ -2,8 +2,6 @@
 
 /*global $, app */
 
-//var app = angular.module('infiniteScroll-directive', []);
-
 app.directive('infiniteScroll', function ($window) {
   return {
     link: function (scope, element, attrs) {
