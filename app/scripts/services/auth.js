d6f3ac53a156b27cf4421808abf98295a9a78eb1
molinamiguel <mm60855@gmail.com>
2014-09-15 14:45:28 -0400

    Removed local username after logout

--- app/scripts/services/auth.js
+++ app/scripts/services/auth.js
@@ -41,12 +41,12 @@ app.factory('AuthService', ['$http', '$window', '$q', 'ENV', function ($http, $w
         $http.post(url).then(
           function () {
             $window.localStorage.removeItem('token');
-            $window.localStorage.removeItem('username');
+            $window.localStorage.removeItem('user.name');
             deferred.resolve();
           },
           function (error) {
             $window.localStorage.removeItem('token');
-            $window.localStorage.removeItem('username');
+            $window.localStorage.removeItem('user.name');
             deferred.resolve();
             // deferred.reject(error.data.error);
           }
